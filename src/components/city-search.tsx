0d7efc82372bdd06b348384492b58b9cb2661242
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-19 16:49:53 +0530

    feat: fix navigation query string in CitySearch and implement CityPage with weather data display

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -42,7 +42,7 @@ const CitySearch = () => {
         });
 
         setOpen(false);
-        navigate(`/city/${name}??lat=${lat}$lon=${lon}`);
+        navigate(`/city/${name}?lat=${lat}$lon=${lon}`);
     };
 
   return (
