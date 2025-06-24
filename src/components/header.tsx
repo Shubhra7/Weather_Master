47043645c8e1520bdaf65827ab6f526e04591b2a
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-24 13:56:17 +0530

    theme logo fixed done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,6 +28,7 @@ const Header = () => {
             <div onClick={()=> setTheme(isDark ? "light":"dark")}
                 className={`flex items-center cursor-pointer transition-transform duration-500
                     ${isDark ? "rotate-180" : "rotate-0"}
+                    mt-2
                     `}
                 >
                 {isDark ? (
